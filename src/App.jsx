291a2ed5af02a88520c8aa4288fd36cd14a8c3d4
Teodor <stevanovicteodor002@gmail.com>
2025-12-16 11:13:59 +0100

    Add basic admin layout using Ant Design

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
-import { Button } from 'antd'
+import React from "react";
+import { Layout, Image } from "antd";
+import LogoAdmin from "./assets/admin-panel-logo.png";
+
+const { Header, Sider, Content } = Layout;
+
 
 function App() {
-
   return (
-    <Button type='primary'>Click Me</Button>
-  )
+    <Layout style={{ minHeight: "100vh" }}>
+      <Header
+        style={{
+          backgroundColor: "#001529",
+          padding: "0 20px",
+        }}
+      >
+        <Image src={LogoAdmin} width={50} height={50} preview={false} />
+      </Header>
+      <Layout>
+        <Sider style={{ backgroundColor: "#001529", color: "white" }}>
+          Left Sider
+        </Sider>
+        <Content style={{ padding: "20px" }}>Main Content</Content>
+      </Layout>
+    </Layout>
+  );
 }
 
-export default App
+export default App;
