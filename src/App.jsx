b400bf3a95542e9aba87977b7bc0cb54e0f25c52
TeodorStevanovic <stevanovicteodor002@gmail.com>
2025-12-17 09:19:34 +0100

    Add interactive sidebar menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
-import React from "react";
-import { Layout, Image } from "antd";
+import React, { useState } from "react";
+import { Layout, Image, Menu } from "antd";
 import LogoAdmin from "./assets/admin-panel-logo.png";
 
 const { Header, Sider, Content } = Layout;
-
+const items = [
+  {
+    key: "1",
+    label: "Dashboard",
+  },
+  {
+    key: "2",
+    label: "Users",
+  },
+  {
+    key: "3",
+    label: "Settings",
+  },
+];
 
 function App() {
+  const [selectItem, setSelectItem] = useState("1");
+
   return (
     <Layout style={{ minHeight: "100vh" }}>
       <Header
@@ -18,7 +33,13 @@ function App() {
       </Header>
       <Layout>
         <Sider style={{ backgroundColor: "#001529", color: "white" }}>
-          Left Sider
+          <Menu
+            onClick={(e) => setSelectItem(e.key)}
+            selectedKeys={[selectItem]}
+            mode="inline"
+            theme="dark"
+            items={items}
+          />
         </Sider>
         <Content style={{ padding: "20px" }}>Main Content</Content>
       </Layout>
