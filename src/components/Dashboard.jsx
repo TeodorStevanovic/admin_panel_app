3104ec0852f79f4a2de1821709e8708ab7a3cc1f
TeodorStevanovic <stevanovicteodor002@gmail.com>
2025-12-25 11:04:39 +0100

    Added Recharts bar chart to Dashboard displaying active and inactive users below existing statistics cards

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,16 +1,28 @@
 import React from "react";
 import { Row, Card, Col, Statistic } from "antd";
-import { UserOutlined } from "@ant-design/icons";
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
 
 const Dashboard = ({ users }) => {
   const activeUsers = users.filter((user) => user.id % 2 === 0).length;
   const inactiveUsers = users.length - activeUsers;
+  const data = [
+    { name: "Active Users", value: activeUsers },
+    { name: "Inactive Users", value: inactiveUsers },
+  ];
   return (
-    <>
+    <div
+      style={{ display: "flex", flexDirection: "column", minHeight: "90vh" }}
+    >
       <Row>
         <Col span={8}>
           <Card>
-            <UserOutlined />
             <Statistic title="Total Users:" value={users.length} />
           </Card>
         </Col>
@@ -25,7 +37,19 @@ const Dashboard = ({ users }) => {
           </Card>
         </Col>
       </Row>
-    </>
+      <Row style={{ marginTop: "auto", paddingTop: 20}}>
+        <Col span={24}>
+          <ResponsiveContainer width="100%" height={300}>
+            <BarChart data={data}>
+              <XAxis dataKey="name" />
+              <YAxis />
+              <Tooltip />
+              <Bar dataKey="value" fill="#1890ff" />
+            </BarChart>
+          </ResponsiveContainer>
+        </Col>
+      </Row>
+    </div>
   );
 };
 
